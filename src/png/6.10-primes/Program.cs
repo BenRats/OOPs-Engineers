86c5b59c36dae1c3ee245112b45586de259e4fbc
penguinite <me@penguinite.dev>
2026-09-14 13:20:51 +0200

    Implement exercise 6.10: Primes
    I don't know if this is right or not tbh

---
+++ src/png/6.10-primes/Program.cs
@@ -0,0 +1,26 @@
+﻿
+for (int num = 0; num < 1000000; num++) {
+    bool is_prime = true;
+
+    // I couldn't grok the exercise itself,
+    // so I decided to look online for any algorithms or ideas
+    // Source: https://stackoverflow.com/a/15743249
+    
+    if (num == 1 || num == 2) {
+        Console.WriteLine(num + " is prime!");
+        continue;
+    }
+
+    var limit = Math.Ceiling(Math.Sqrt(num));
+    for (int i = 2; i <= limit; i++) {
+        if (num % i == 0) {
+            is_prime = false;
+        }
+    }
+
+    if (is_prime)
+        Console.WriteLine(num + " is prime!");
+    // just don't print non-prime numbers...
+    // Best hack ever: Use ternary operators for strings.
+    //Console.WriteLine(num + (is_prime ? " is prime!" : " is not prime!"));
+}
