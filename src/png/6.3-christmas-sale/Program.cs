4892e2f66a80213ae6c21b4bcb469aa7e54cf75c
penguinite <me@penguinite.dev>
2026-09-14 12:27:22 +0200

    Implement exercise 6.3: Christmas sale
    Yes, this is slightly lazily done but there's no way we're ready for time handling just yet.
    The previous UNIX Epoch-based exercise was pain :(

---
+++ src/png/6.3-christmas-sale/Program.cs
@@ -0,0 +1,13 @@
+﻿//int date = 21816000;
+double entryPrice = 599.95;
+double rebate = 0.3;
+
+bool isChristmas = false; // honor-based system :)
+
+double price = entryPrice;
+if (isChristmas) {
+    Console.WriteLine("Applying Christmas rebate, Merry Christmas!");
+    price = price * (1.0 - rebate);
+}
+Console.WriteLine("Price: " + price);
+
