233a20771a712a461337910e208e267265286136
penguinite <me@penguinite.dev>
2026-09-14 12:56:12 +0200

    Implement exercise 6.9: Areas of circles
    nice

---
+++ src/png/6.9-areas-of-circles/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+for (int i = 1; i <= 5; i += 2) {
+    double area = 3.14159 * (i * i); // (i * i) is my way of doing r^2
+    Console.WriteLine("Area of circle with radius " + i + " is " + area);
+}
