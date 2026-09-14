72e5c5b0a3ce323cf129c1962fe9939067ecd8e7
penguinite <me@penguinite.dev>
2026-09-14 12:21:31 +0200

    Implement exercise 5.20: Dice

---
+++ src/png/5.20-dice/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+var rnd = new Random();
+int diceRoll = rnd.Next(1, 6);
+bool biggerThan2 = (diceRoll >= 3 ? true : false);
+
+Console.WriteLine("Dice roll: " + diceRoll);
+Console.WriteLine("Bigger than 2? " + biggerThan2);
