7cb66fca0eaffe7cd20ca12823190e55eba43183
penguinite <me@penguinite.dev>
2026-09-14 12:42:32 +0200

    Implement exercise 6.5: Holidays

---
+++ src/png/6.5-holidays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+int userMonth;
+
+Console.Write("Type a month from 1 to 12: ");
+Int32.TryParse(Console.ReadLine(), out userMonth);
+
+// basic checks first
+if (userMonth < 1 || userMonth > 12) {
+    Console.WriteLine("Invalid month, must be from 1 to 12!");
+    Environment.Exit(0);
+}
+
+switch (userMonth) {
+    case 4:
+        Console.WriteLine("Spring Holiday!");
+        break;
+
+    case 7:
+        Console.WriteLine("Summer Holiday!");
+        break;
+
+    case 8:
+        Console.WriteLine("Summer Holiday!");
+        break;
+        
+    case 10:
+        Console.WriteLine("Autumn Holiday!");
+        break;
+
+    default:
+        Console.WriteLine("Hard work");
+        break;
+}
